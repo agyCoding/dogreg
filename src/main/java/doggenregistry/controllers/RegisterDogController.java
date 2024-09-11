c2c54763858b60fe5f88fe0d973df3f661daae3d
agyCoding <agata.olaussen@gmail.com>
2024-09-11 11:19:19 +0200

    Moved showAlert method to a new Utils file (general method should be "hosted" in common project files).

--- src/main/java/doggenregistry/controllers/RegisterDogController.java
+++ src/main/java/doggenregistry/controllers/RegisterDogController.java
@@ -1,6 +1,8 @@
 package doggenregistry.controllers;
 
+import doggenregistry.models.User;
 import doggenregistry.services.DogManager;
+import doggenregistry.services.UserManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
@@ -10,6 +12,8 @@ public class RegisterDogController {
     DogManager dogManager = DogManager.getInstance();
 
     private String userName;
+
+    private int userID;
 
     @FXML
     private TextField nameTextField;
@@ -29,7 +33,13 @@ public class RegisterDogController {
         dogManager.populateIsFemaleBox(isFemaleBox);
     }
 
+    // Save userName passed from previous scene, so it can be used to retrieve userID
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    // Assign userID based on the userName passed from previous scene, for further use in dog registration
+    public void setUserID(String userName) {
+        this.userID = UserManager.getInstance().fetchUserIDFromDatabase(userName);
     }
 }
